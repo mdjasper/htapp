c79da38477a0c48427c9bbaee560993ddd3baf55
Michael Jasper <mdjasper@gmail.com>
2014-01-31 11:37:23 -0700

    merged some files

---
+++ footer.php
@@ -0,0 +1,11 @@
+	<footer class="footer">
+			<ul>
+				<li>Results</li>
+				<li>Profile</li>
+			</ul>
+		</footer>
+
+		<script defer src="/assets/scripts/jquery-1.8.3.min.js"></script>
+		<script defer src="/assets/scripts/main.js"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8"/>
+
+	<title></title>
+
+	<meta name="description" content=""/>
+	<meta name="author" content=""/>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!-- May need this line alternatively for touch events:
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"/>
+	-->
+
+	<link rel="stylesheet" href="/assets/styles/main.css" media="all"/>
+	<link rel="shortcut icon" href="/assets/images/favicon.ico"/>
+
+	<!--[if lt IE 9]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<script src="/assets/scripts/respond.min.js"></script>
+		<script src="/assets/scripts/html5shiv.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<header class="header">
+		<nav class="nav">
+			<ul>
+				<li>Results</li>
+				<li>Profile</li>
+			</ul>
+		</nav>
+	</header>
