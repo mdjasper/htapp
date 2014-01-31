f0c41772332680f019199a7d5007fda53db6b61e
Michael Jasper <mdjasper@gmail.com>
2014-01-31 12:46:05 -0700

    reworked more paths

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? include_once("/modules/header.php"); ?>
+<? include_once("modules/header.php"); ?>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
@@ -8,4 +8,4 @@
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
 
-<? include_once("/modules/footer.php"); ?>
+<? include_once("modules/footer.php"); ?>
