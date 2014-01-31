52fc8dc6b9e2cf0a501e1f154dd3a1a333b83b8e
Michael Jasper <mdjasper@gmail.com>
2014-01-31 13:31:12 -0700

    leaderboard report css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 			<td>Rank</td>
 			<td>Badges</td>
 			<td>Name</td>
+			<td>Points</td>
 		</tr>
 	</thead>
 	<tr class="leaderboard--person">
@@ -14,6 +15,7 @@
 			<div class="badge badge--sun"></div><div class="badge badge--sun"></div>
 		</td>
 		<td>Sister Janice Eunice</td>
+		<td>10,033</td>
 	</tr>
 
 	<tr class="leaderboard--person">
@@ -22,6 +24,7 @@
 			<div class="badge badge--sun"></div><div class="badge badge--moon"></div>
 		</td>
 		<td>Brother Peter Jones</td>
+		<td>577</td>
 	</tr>
 
 	<tr class="leaderboard--person">
@@ -30,14 +33,52 @@
 			<div class="badge badge--moon"></div><div class="badge badge--star"></div>
 		</td>
 		<td>Brother Jack Smith</td>
+		<td>204</td>
 	</tr>
 
 	<tr class="leaderboard--person">
-		<td>3</td>
+		<td>4</td>
 		<td>
 			<div class="badge badge--star"></div>
 		</td>
 		<td>Brother Jack Smith</td>
+		<td>14</td>
+	</tr>
+
+	<tr class="leaderboard--person">
+		<td>5</td>
+		<td>
+			<div class="badge badge--star"></div>
+		</td>
+		<td>Brother Bob Billy</td>
+		<td>10</td>
+	</tr>
+
+	<tr class="leaderboard--person">
+		<td>6</td>
+		<td>
+			
+		</td>
+		<td>Brother John Mackey</td>
+		<td>4</td>
+	</tr>
+
+	<tr class="leaderboard--person">
+		<td>6</td>
+		<td>
+			
+		</td>
+		<td>Brother Colby Robertson</td>
+		<td>4</td>
+	</tr>
+
+	<tr class="leaderboard--person">
+		<td>6</td>
+		<td>
+			
+		</td>
+		<td>Brother Brian Thorpe</td>
+		<td>4</td>
 	</tr>
 
 </table>
--- report.php
+++ report.php
@@ -1,24 +1,212 @@
 <? include_once("modules/header.php"); ?>
-
-<table class="report">
+<div class="report">
+<table>
 	<tr>
+		<td>
+			<span class="report--name">I visited the <i>Jones Family</i></span>
+		</td>
 		<td class="report--check">
 			<input type="checkbox" >
 		</td>
-		<td>
-			<span class="report--name">{{family-name}}</span>
+	</tr>
+	<tr>
+		<td>Type of Visit
 		</td>
 		<td>
-			<select class="report--result">
-				<option>{{option}}</option>
+			<select name="menvisittype">
+				<option value="0" selected="selected">Missing in Action</option>
+				<option value="1">Saw in the hallway</option>
+				<option value="2">Over the fence</option>
+				<option value="3">Drive By</option>
+				<option value="41">Halloween</option>
+				<option value="51">New Year's Eve</option>
+				<option value="61">Wave</option>
+				<option value="71">Email</option>
+				<option value="81">Phone Call</option>
+				<option value="91">High Five @ Church</option>
+				<option value="191">Thought taped to the door</option>
+				<option value="291">Crafty Gift</option>
+				<option value="391">Cookies</option>
+				<option value="491">Full Visit</option>
 			</select>
 		</td>
 	</tr>
 	<tr>
-		<td colspan="3">
-			<textarea class="report--comments"></textarea>
+		<td>Home Teaching Excuses</td>
+		<td>
+			<select name="menexcuses">
+				<option value="1" selected="selected">No excuse</option>
+				<option value="0">Wern't home on last day of the month</option>
+				<option value="2">I'm the Bishop</option>
+				<option value="3">BYU/Utah lost, family wouldn't let us in.</option>
+				<option value="41">Superbowl</option>
+				<option value="51">Forgot who my familes are</option>
+				<option value="61">Ensign didn't come this month</option>
+				<option value="71">Too many holidays</option>
+				<option value="81">Family wasn't at church</option>
+				<option value="91">My companion could't go</option>
+				<option value="191">Family was trick or treating/New Year's Eve party</option>
+				<option value="191">Other (specify below)</option>
+			</select>
 		</td>
 	</tr>
+	<tr>
+		<td>Family Status</td>
+		<td>
+			<select name="mfamilystatus">
+				<option value="333" selected="selected">Did not contact</option>
+				<option value="1">Fine</option>
+				<option value="0">Wonderful</option>
+				<option value="2">Doin' good</option>
+				<option value="3">Great</option>
+				<option value="41">No problems</option>
+				<option value="51">Happy</option>
+				<option value="61">Needs help moving tomorrow</option>
+			</select>
+		</td>
+	</tr>
+
 </table>
+</div>
+
+<div class="report">
+<table>
+	<tr>
+		<td>
+			<span class="report--name">I visited the <i>Smith Family</i></span>
+		</td>
+		<td class="report--check">
+			<input type="checkbox" >
+		</td>
+	</tr>
+	<tr>
+		<td>Type of Visit
+		</td>
+		<td>
+			<select name="menvisittype">
+				<option value="0" selected="selected">Missing in Action</option>
+				<option value="1">Saw in the hallway</option>
+				<option value="2">Over the fence</option>
+				<option value="3">Drive By</option>
+				<option value="41">Halloween</option>
+				<option value="51">New Year's Eve</option>
+				<option value="61">Wave</option>
+				<option value="71">Email</option>
+				<option value="81">Phone Call</option>
+				<option value="91">High Five @ Church</option>
+				<option value="191">Thought taped to the door</option>
+				<option value="291">Crafty Gift</option>
+				<option value="391">Cookies</option>
+				<option value="491">Full Visit</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Home Teaching Excuses</td>
+		<td>
+			<select name="menexcuses">
+				<option value="1" selected="selected">No excuse</option>
+				<option value="0">Wern't home on last day of the month</option>
+				<option value="2">I'm the Bishop</option>
+				<option value="3">BYU/Utah lost, family wouldn't let us in.</option>
+				<option value="41">Superbowl</option>
+				<option value="51">Forgot who my familes are</option>
+				<option value="61">Ensign didn't come this month</option>
+				<option value="71">Too many holidays</option>
+				<option value="81">Family wasn't at church</option>
+				<option value="91">My companion could't go</option>
+				<option value="191">Family was trick or treating/New Year's Eve party</option>
+				<option value="191">Other (specify below)</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Family Status</td>
+		<td>
+			<select name="mfamilystatus">
+				<option value="333" selected="selected">Did not contact</option>
+				<option value="1">Fine</option>
+				<option value="0">Wonderful</option>
+				<option value="2">Doin' good</option>
+				<option value="3">Great</option>
+				<option value="41">No problems</option>
+				<option value="51">Happy</option>
+				<option value="61">Needs help moving tomorrow</option>
+			</select>
+		</td>
+	</tr>
+
+</table>
+</div>
+
+<div class="report">
+<table>
+	<tr>
+		<td>
+			<span class="report--name">I visited the <i>Roberts Family</i></span>
+		</td>
+		<td class="report--check">
+			<input type="checkbox" >
+		</td>
+	</tr>
+	<tr>
+		<td>Type of Visit
+		</td>
+		<td>
+			<select name="menvisittype">
+				<option value="0" selected="selected">Missing in Action</option>
+				<option value="1">Saw in the hallway</option>
+				<option value="2">Over the fence</option>
+				<option value="3">Drive By</option>
+				<option value="41">Halloween</option>
+				<option value="51">New Year's Eve</option>
+				<option value="61">Wave</option>
+				<option value="71">Email</option>
+				<option value="81">Phone Call</option>
+				<option value="91">High Five @ Church</option>
+				<option value="191">Thought taped to the door</option>
+				<option value="291">Crafty Gift</option>
+				<option value="391">Cookies</option>
+				<option value="491">Full Visit</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Home Teaching Excuses</td>
+		<td>
+			<select name="menexcuses">
+				<option value="1" selected="selected">No excuse</option>
+				<option value="0">Wern't home on last day of the month</option>
+				<option value="2">I'm the Bishop</option>
+				<option value="3">BYU/Utah lost, family wouldn't let us in.</option>
+				<option value="41">Superbowl</option>
+				<option value="51">Forgot who my familes are</option>
+				<option value="61">Ensign didn't come this month</option>
+				<option value="71">Too many holidays</option>
+				<option value="81">Family wasn't at church</option>
+				<option value="91">My companion could't go</option>
+				<option value="191">Family was trick or treating/New Year's Eve party</option>
+				<option value="191">Other (specify below)</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Family Status</td>
+		<td>
+			<select name="mfamilystatus">
+				<option value="333" selected="selected">Did not contact</option>
+				<option value="1">Fine</option>
+				<option value="0">Wonderful</option>
+				<option value="2">Doin' good</option>
+				<option value="3">Great</option>
+				<option value="41">No problems</option>
+				<option value="51">Happy</option>
+				<option value="61">Needs help moving tomorrow</option>
+			</select>
+		</td>
+	</tr>
+
+</table>
+</div>
 
 <? include_once("modules/footer.php"); ?>
