0f2afab211153d16525914876a13434a90cfb162
clearwavedesigns <adamcarson.io@gmail.com>
2014-01-31 13:14:42 -0700

    updates

--- modules/footer.php
+++ modules/footer.php
@@ -5,6 +5,7 @@
 		<li><a href="profile.php">My Profile</a></li>
 		<li><a href="report.php">Report</a></li>
 	</ul>
+	<p>&copy; 2014 The Teal Team</p>
 </footer>
 
 <script src="assets/scripts/jquery-1.8.3.min.js"></script>
