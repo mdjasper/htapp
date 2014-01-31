c74812f8efb0493327154556c6b80807a42bd5f2
clearwavedesigns <adamcarson.io@gmail.com>
2014-01-31 12:11:04 -0700

    More styles on homepage
    .

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <? include_once("/modules/header.php"); ?>
 
-
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, blanditiis eaque ut ea iste delectus deleniti veniam corporis animi molestias cupiditate fugit veritatis culpa quae laudantium cum facilis labore consequuntur!</p>
 
 <? include_once("/modules/footer.php"); ?>
