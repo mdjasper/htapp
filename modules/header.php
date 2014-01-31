69328f3fcbdd9df8831c5b5438a440932b89188d
clearwavedesigns <adamcarson.io@gmail.com>
2014-01-31 13:30:55 -0700

    New logo
    .

--- modules/header.php
+++ modules/header.php
@@ -13,29 +13,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<!-- May need this line alternatively for touch events:
 	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"/>
-	-->
+-->
 
-	<link rel="stylesheet" href="assets/styles/main.css" media="all"/>
-	<link rel="stylesheet" href="assets/styles/report.css" media="all"/>
-	<link rel="stylesheet" href="assets/styles/leaderboard.css" media="all"/>
-	<link rel="shortcut icon" href="assets/images/favicon.ico"/>
+<link rel="stylesheet" href="assets/styles/main.css" media="all"/>
+<link rel="stylesheet" href="assets/styles/report.css" media="all"/>
+<link rel="stylesheet" href="assets/styles/leaderboard.css" media="all"/>
+<link rel="shortcut icon" href="assets/images/logo.png"/>
 
 	<!--[if lt IE 9]>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<script src="/assets/scripts/respond.min.js"></script>
 		<script src="/assets/scripts/html5shiv.js"></script>
-	<![endif]-->
-</head>
+		<![endif]-->
+	</head>
 
-<body>
-	<header class="header">
-		<nav class="nav">
-			<ul>
-				<li><a href="index.php">Leadboard</a></li>
-				<li><a href="profile.php">My Profile</a></li>
-				<li><a href="report.php">Report</a></li>
-			</ul>
-		</nav>
-	</header>
-<div class="main">
-	
+	<body>
+		<header class="header">
+			<figure>
+				<a href="index.php"><img src="assets/images/logo.png" alt="Logo"/></a>
+			</figure>
+			<nav class="nav">
+				<ul>
+					<li><a href="index.php">Leadboard</a></li>
+					<li><a href="profile.php">My Profile</a></li>
+					<li><a href="report.php">Report</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div class="main">
+			
