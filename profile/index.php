686a400839415a5901fbaf03be7a2904ec67cd1b
steinerdj <steinerdj@ldschurch.org>
2014-01-31 12:26:58 -0700

    womans chart

--- profile/index.php
+++ profile/index.php
@@ -1,5 +1,6 @@
 <? include_once("/modules/header.php"); ?>
 
 <div id="manchart" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<div id="womanchart" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 <? include_once("/modules/footer.php"); ?>
